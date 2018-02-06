a750151b625d88d430ba0b00af7cc0413eecf353
Caroline Lee <caroline@chatkit.com>
2018-02-06 10:28:52 -0500

    Back to service accounts

--- google2pandas/_panalysis_ga.py
+++ google2pandas/_panalysis_ga.py
@@ -50,7 +50,7 @@ class GoogleAnalyticsQuery(GoogleServiceReader):
 
             https://developers.google.com/analytics/devguides/reporting/core/v4/parameters
         '''
-        super(GoogleAnalyticsQuery).__init__()
+        super(GoogleAnalyticsQuery, self).__init__()
         print(self._scope_)
         self._service = self._init_service(secrets_location)
 
